1a6f8a8f4dd9c1b39c6fb6105833e6d6396d35ad
Shaun Yow <shaun.yow@123rf.com>
2021-06-10 21:32:46 +0800

    minor change

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 
 import Amplify, { Auth } from "aws-amplify";
 import { withAuthenticator } from '@aws-amplify/ui-react'
-import awsExports from "src/aws-exports";
+import awsExports from "aws-exports";
 import 'bootstrap/dist/css/bootstrap.css';
 
 Amplify.configure(awsExports);
